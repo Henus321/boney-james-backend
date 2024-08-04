1a38b792fd2c7cb7e78690dfa18ac8302c50878e
Alexander Erkhov <tyrantbud@yandex.ru>
2024-08-04 21:10:43 +0300

    feat/app-tune: Add http-req logger, cookie parse and new start scripts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
 import express from "express";
 import coatRoutes from "./routes/coatRoutes";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 import { AppError } from "./utils/appError";
 
 const app = express();
 
+// Logging in development
+if (process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
-// TODO coockie parser
+app.use(cookieParser());
 
 // Routes
 app.use("/api/v1/coat", coatRoutes);
