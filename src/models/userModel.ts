237aa23547827b7a066a985de2eeca83be595790
Alexander Erkhov <tyrantbud@yandex.ru>
2024-08-05 21:33:17 +0300

    feat/auth: Add user model

---
+++ src/models/userModel.ts
@@ -0,0 +1,76 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: [true, "Please tell us your name!"],
+            trim: true,
+            maxlength: [
+                40,
+                "A username must have less or equal then 40 characters!",
+            ],
+        },
+        email: {
+            type: String,
+            required: [true, "Please provide your email"],
+            trim: true,
+            maxlength: [
+                40,
+                "An email must have less or equal then 40 characters!",
+            ],
+            unique: true,
+            lowercase: true,
+            validate: [validator.isEmail, "Please provide a valid email"],
+        },
+        password: {
+            type: String,
+            required: [true, "Please provide a password"],
+            trim: true,
+            minlength: 6,
+            maxlength: [
+                40,
+                "A password must have less or equal then 40 characters!",
+            ],
+            select: false,
+        },
+        passwordConfirm: {
+            type: String,
+            required: [true, "Please confirm your password"],
+            trim: true,
+            validate: {
+                validator: function (el) {
+                    return el === this.password;
+                },
+                message: "Passwords are not the same!",
+            },
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    // Hash the password with cost of 12
+    this.password = await bcrypt.hash(this.password, 12);
+
+    // Delete passwordConfirm field
+    this.passwordConfirm = undefined as any;
+    next();
+});
+
+userSchema.methods.correctPassword = async function (
+    candidatePassword: string,
+    userPassword: string
+) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
