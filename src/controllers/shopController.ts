11bc48d1f03ecb11529bb4d5aa5ddfd50099b1a0
Alexander Erkhov <tyrantbud@yandex.ru>
2024-08-18 16:11:50 +0300

    fix/shop: Add query params get all shops endpoint

--- src/controllers/shopController.ts
+++ src/controllers/shopController.ts
@@ -1,14 +1,31 @@
 import {
     deleteOne,
-    getAll,
     getOne,
     createOne,
     updateOne,
 } from "../utils/handlerFactory";
 import Shop from "../models/shopModel";
+import asyncHandler from "express-async-handler";
 
-export const getAllShops = getAll(Shop);
 export const getShop = getOne(Shop);
 export const createShop = createOne(Shop);
 export const updateShop = updateOne(Shop);
 export const deleteShop = deleteOne(Shop);
+
+export const getAllShops = asyncHandler(async (req, res, next) => {
+    const { city, type } = req.query;
+    const query = {};
+
+    if (city) Object.assign(query, { city });
+    if (type) Object.assign(query, { types: type });
+
+    const doc = await Shop.find(query);
+
+    res.status(200).json({
+        status: "success",
+        results: doc.length,
+        data: {
+            data: doc,
+        },
+    });
+});
